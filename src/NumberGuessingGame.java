46377366651593432e3c777ee61ba124cbc4f447
Dante <dante.collier.2022@student.lhschools.org>
2019-04-04 14:02:28 -0400

    Shortened code and created loop

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,28 +1,32 @@
+
+
 import java.util.Scanner;
 
 public class NumberGuessingGame {
 
     public static void main(String[] args) {
+        System.out.println("Guess a number between 1 and 100.");
+        checkNumber();
+        }
+        public static void checkNumber() {
         int pickedNumber = (int) (Math.random() * 100);
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Guess a number between 1 and 100.");
         int guess = keyboard.nextInt();
-        checkNumber(pickedNumber,guess);
-        System.out.println("Guess again, you have two mor tries.");
-        checkNumber(pickedNumber,guess);
-        System.out.println("Guess ");
-    }
 
-    public static void checkNumber(int pickedNumber, int guess) {
-        if (pickedNumber > guess){
-            System.out.println("Your number is too low.");
+        while (guess < pickedNumber||guess > pickedNumber){
+            if (pickedNumber > guess) {
+            System.out.println("Your number is too low, guess again.");
+            guess = keyboard.nextInt();
             }
-        if (pickedNumber < guess){
-            System.out.println("Your number is too high.");
-        }
-        if (pickedNumber == guess){
+            if (pickedNumber < guess) {
+            System.out.println("Your number is too high, guess again.");
+            guess = keyboard.nextInt();
+            }
+            if (pickedNumber == guess) {
             System.out.println("You have correctly guessed the number " + pickedNumber + ".");
             System.exit(0);
+            }
+
         }
     }
 }
